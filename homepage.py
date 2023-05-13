f941d836e3750e15710c46bef6eda07c98904b08
RiosVal <127972275+RiosVal@users.noreply.github.com>
2023-05-13 18:22:08 -0500

    Add files via upload

---
+++ homepage.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+st.title("Bienvenido a TDA's learning")
+
+arboles = "Árboles"
+grafos = "Grafos"
+
+option = st.selectbox(
+    "Escoge el tema que quieres aprender",
+    ('-', arboles, grafos)
+)
+
+if st.button("Enviar"):
+    if option == arboles:
+        switch_page("arboles")
