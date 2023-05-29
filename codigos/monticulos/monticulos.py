53832a33eef1a347e956a4e69c07c0ae612e1765
Chologalactico <116129679+Chologalactico@users.noreply.github.com>
2023-05-29 14:45:10 -0500

    Update monticulos.py

--- codigos/monticulos/monticulos.py
+++ codigos/monticulos/monticulos.py
@@ -130,173 +130,157 @@ def tda_monticulos():
     opcion = st.selectbox(
         "",
         (
-            "__init__(self,tamaño)",
-            "Crear_monticulo(self,tamaño)",
-            "agregar(self,dato),quitar(self),flotar(self,indice)",
-            "Hundir(self,indice)",
-            "ordenar y revisar(self)",
-            "monticulizar(self)",
-            'Codigo Completo')
+            " ",
+            "Estructura",
+            "Eventos",
+            "Codigo completo"
+            )
     )
 
-    if opcion == "__init__(self,tamaño)":
-        st.write('Crear una clase Monticulo',unsafe_allow_html=True)
-        code = '''
-      class Monticulo:
-        def __init__(self, tamaño):
-            self.heap = [None] * tamaño
-            self.size = 0'''
-    elif opcion == "Crear_monticulo(self,tamaño)":
-        st.write('- Crea y devuelve un montículo vacío con la cantidad de elementos,determinado por el tamaño ingresado',unsafe_allow_html=True)
-        code = '''
-        def crear_monticulo(self, tamaño):
-        self.heap = [None] * tamaño
-        self.size = 0'''
-    elif opcion == "agregar(self,dato),quitar(self),flotar(self,indice)":
-        st.write('''- Agregar-Inserta un elemento al final del montículo y luego lo flota hasta que dicho elemento cumpla la propiedad de orden
-                    - Quitar-Quita y devuelve el máximo o mínimo elemento del montículo dependiendo de su tipo, es decir el dato que se ubica en la raíz del árbol , y en su lugar se coloca 
-                            el último elemento del montículo y luego lo hunde hasta que cumpla la propiedad de orden
-                    - Flotar-Flota el dato almacenado en el montículo desde el índice indicado 
-                            hasta que cumpla la propiedad de orden''', unsafe_allow_html=True)
-        code = '''
-   def agregar(self, dato):
-        if self.size >= len(self.heap):
-            # El montículo está lleno
-            return
-        self.heap[self.size] = dato
-        self.flotar(self.size)
-        self.size += 1
+    if opcion == "Estructura":
+        codeEstructura =  '''
+                class Monticulo:
+                    def __init__(self, tamaño):
+                        self.heap = [None] * tamaño
+                        self.size = 0
 
-    def quitar(self):
-        if self.size == 0:
-            # El montículo está vacío
-            return None
-        dato = self.heap[0]
-        self.size -= 1
-        self.heap[0] = self.heap[self.size]
-        self.hundir(0)
-        return dato
+                    def crear_monticulo(self, tamaño):
+                        self.heap = [None] * tamaño
+                        self.size = 0
+                '''
+        st.code(codeEstructura, language='python')
 
-    def flotar(self, índice):
-        padre = (índice - 1) // 2
-        while índice > 0 and self.heap[índice] > self.heap[padre]:
-            self.heap[índice], self.heap[padre] = self.heap[padre], self.heap[índice]
-            índice = padre
-            padre = (índice - 1) // 2'''
-    elif opcion == "Hundir(self,indice)":
-        st.write('- Hunde el dato almacenado en el montículo desde el índice indicado hasta que cumpla la propiedad de orden', unsafe_allow_html=True)
-        code = '''
-        def hundir(self, índice):
-        hijo_izquierdo = 2 * índice + 1
-        hijo_derecho = 2 * índice + 2
-        índice_mayor = índice
+    elif opcion == "Eventos":
+        codeEventos = '''
+            def agregar(self, dato):
+                    if self.size >= len(self.heap):
+                        # El montículo está lleno
+                        return
+                    self.heap[self.size] = dato
+                    self.flotar(self.size)
+                    self.size += 1
 
-        if hijo_izquierdo < self.size and self.heap[hijo_izquierdo] > self.heap[índice_mayor]:
-            índice_mayor = hijo_izquierdo
-        if hijo_derecho < self.size and self.heap[hijo_derecho] > self.heap[índice_mayor]:
-            índice_mayor = hijo_derecho
+            def quitar(self):
+                if self.size == 0:
+                    # El montículo está vacío
+                    return None
+                dato = self.heap[0]
+                self.size -= 1
+                self.heap[0] = self.heap[self.size]
+                self.hundir(0)
+                return dato
 
-        if índice_mayor != índice:
-            self.heap[índice], self.heap[índice_mayor] = self.heap[índice_mayor], self.heap[índice]
-            self.hundir(índice_mayor)
-'''
-    elif opcion == "ordenar y revisar(self)":
-        st.write('''-  Devuelve verdadero (true) si el montículo no contiene elementos
-                    -  Devuelve verdadero (true) si el montículo no puede almacenar más elementos
-                    -  Devuelve la cantidad de elementos del montículo''', 
-                unsafe_allow_html=True)
-        code = '''
+            def flotar(self, índice):
+                padre = (índice - 1) // 2
+                while índice > 0 and self.heap[índice] > self.heap[padre]:
+                    self.heap[índice], self.heap[padre] = self.heap[padre], self.heap[índice]
+                    índice = padre
+                    padre = (índice - 1) // 2
+
+            def hundir(self, índice):
+                hijo_izquierdo = 2 * índice + 1
+                hijo_derecho = 2 * índice + 2
+                índice_mayor = índice
+
+                if hijo_izquierdo < self.size and self.heap[hijo_izquierdo] > self.heap[índice_mayor]:
+                    índice_mayor = hijo_izquierdo
+                if hijo_derecho < self.size and self.heap[hijo_derecho] > self.heap[índice_mayor]:
+                    índice_mayor = hijo_derecho
+
+                if índice_mayor != índice:
+                    self.heap[índice], self.heap[índice_mayor] = self.heap[índice_mayor], self.heap[índice]
+                    self.hundir(índice_mayor)
+
             def montículo_vacio(self):
-        return self.size == 0
+                return self.size == 0
 
-    def montículo_lleno(self):
-        return self.size == len(self.heap)
+            def montículo_lleno(self):
+                return self.size == len(self.heap)
 
-    def tamaño(self):
-        return self.size'''
+            def tamaño(self):
+                return self.size
+                
+            @classmethod
+            def monticulizar(cls, elementos):
+                tamaño = len(elementos)
+                monticulo = cls(tamaño)
+                monticulo.heap = elementos
+                monticulo.size = tamaño
 
-    elif opcion == "monticulizar(self)":
-        st.write(' Convierte un vector de elementos en un montículo',unsafe_allow_html=True)
-        code = '''
-           @classmethod
-    def monticulizar(cls, elementos):
-        tamaño = len(elementos)
-        monticulo = cls(tamaño)
-        monticulo.heap = elementos
-        monticulo.size = tamaño
+                for i in range(tamaño // 2 - 1, -1, -1):
+                    monticulo.hundir(i)
 
-        for i in range(tamaño // 2 - 1, -1, -1):
-            monticulo.hundir(i)
+                return monticulo'''
+        st.code(codeEventos, language='python')
 
-        return monticulo'''
-    elif opcion == "Codigo Completo":
-        code = '''
-           class Monticulo:
-    def __init__(self, tamaño):
-        self.heap = [None] * tamaño
-        self.size = 0
+    elif opcion == "Codigo completo":
+        codeCompleto = '''
+            class Monticulo:
+            def __init__(self, tamaño):
+                self.heap = [None] * tamaño
+                self.size = 0
 
-    def crear_monticulo(self, tamaño):
-        self.heap = [None] * tamaño
-        self.size = 0
+            def crear_monticulo(self, tamaño):
+                self.heap = [None] * tamaño
+                self.size = 0
 
-    def agregar(self, dato):
-        if self.size >= len(self.heap):
-            # El montículo está lleno
-            return
-        self.heap[self.size] = dato
-        self.flotar(self.size)
-        self.size += 1
+            def agregar(self, dato):
+                if self.size >= len(self.heap):
+                    # El montículo está lleno
+                    return
+                self.heap[self.size] = dato
+                self.flotar(self.size)
+                self.size += 1
 
-    def quitar(self):
-        if self.size == 0:
-            # El montículo está vacío
-            return None
-        dato = self.heap[0]
-        self.size -= 1
-        self.heap[0] = self.heap[self.size]
-        self.hundir(0)
-        return dato
+            def quitar(self):
+                if self.size == 0:
+                    # El montículo está vacío
+                    return None
+                dato = self.heap[0]
+                self.size -= 1
+                self.heap[0] = self.heap[self.size]
+                self.hundir(0)
+                return dato
 
-    def flotar(self, índice):
-        padre = (índice - 1) // 2
-        while índice > 0 and self.heap[índice] > self.heap[padre]:
-            self.heap[índice], self.heap[padre] = self.heap[padre], self.heap[índice]
-            índice = padre
-            padre = (índice - 1) // 2
+            def flotar(self, índice):
+                padre = (índice - 1) // 2
+                while índice > 0 and self.heap[índice] > self.heap[padre]:
+                    self.heap[índice], self.heap[padre] = self.heap[padre], self.heap[índice]
+                    índice = padre
+                    padre = (índice - 1) // 2
 
-    def hundir(self, índice):
-        hijo_izquierdo = 2 * índice + 1
-        hijo_derecho = 2 * índice + 2
-        índice_mayor = índice
+            def hundir(self, índice):
+                hijo_izquierdo = 2 * índice + 1
+                hijo_derecho = 2 * índice + 2
+                índice_mayor = índice
 
-        if hijo_izquierdo < self.size and self.heap[hijo_izquierdo] > self.heap[índice_mayor]:
-            índice_mayor = hijo_izquierdo
-        if hijo_derecho < self.size and self.heap[hijo_derecho] > self.heap[índice_mayor]:
-            índice_mayor = hijo_derecho
+                if hijo_izquierdo < self.size and self.heap[hijo_izquierdo] > self.heap[índice_mayor]:
+                    índice_mayor = hijo_izquierdo
+                if hijo_derecho < self.size and self.heap[hijo_derecho] > self.heap[índice_mayor]:
+                    índice_mayor = hijo_derecho
 
-        if índice_mayor != índice:
-            self.heap[índice], self.heap[índice_mayor] = self.heap[índice_mayor], self.heap[índice]
-            self.hundir(índice_mayor)
+                if índice_mayor != índice:
+                    self.heap[índice], self.heap[índice_mayor] = self.heap[índice_mayor], self.heap[índice]
+                    self.hundir(índice_mayor)
 
-    def montículo_vacio(self):
-        return self.size == 0
+            def montículo_vacio(self):
+                return self.size == 0
 
-    def montículo_lleno(self):
-        return self.size == len(self.heap)
+            def montículo_lleno(self):
+                return self.size == len(self.heap)
 
-    def tamaño(self):
-        return self.size
+            def tamaño(self):
+                return self.size
 
-    @classmethod
-    def monticulizar(cls, elementos):
-        tamaño = len(elementos)
-        monticulo = cls(tamaño)
-        monticulo.heap = elementos
-        monticulo.size = tamaño
+            @classmethod
+            def monticulizar(cls, elementos):
+                tamaño = len(elementos)
+                monticulo = cls(tamaño)
+                monticulo.heap = elementos
+                monticulo.size = tamaño
 
-        for i in range(tamaño // 2 - 1, -1, -1):
-            monticulo.hundir(i)
+                for i in range(tamaño // 2 - 1, -1, -1):
+                    monticulo.hundir(i)
 
-        return monticulo'''
-    st.code(code, language='python')
+                return monticulo'''
+        st.code(codeCompleto, language='python')
