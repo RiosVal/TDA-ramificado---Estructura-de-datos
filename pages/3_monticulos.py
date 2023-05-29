8b9f07a2672781221ed3e1382980975a430c8f19
RiosVal <riosh.valeria@gmail.com>
2023-05-29 07:41:53 -0500

    Update: 3_monticulos.py

--- pages/3_monticulos.py
+++ pages/3_monticulos.py
@@ -18,7 +18,7 @@ def intro():
             Los montículos (Heaps) son estructuras de datos que permite la extracción de los
             mismos de forma ordenada. Existen dos tipos: los montículos máximos, que
             permite ex  traer los elementos en orden decreciente y los montículos mínimos, en
-            que la extracción se inicia por el más pequeño y termina con el valor máximo.
+            que la extracción se inicia por el más pequeño y termina con el valor máximo.[2]
             </p></div>
         """,
         unsafe_allow_html=True
@@ -50,12 +50,12 @@ def monticulosMaxMin():
         st.write("""<div style="text-align: center;"><h2>Monticulos Minimos</h2></div>""", unsafe_allow_html=True)
         st.write("""<div style="text-align: justify;">Un montículo mínimo es similar a un montículo máximo, pero en este caso, el valor de cada nodo es 
                     menor o igual que el valor de sus nodos hijos. El nodo raíz tiene el valor mínimo en todo el montículo y, para 
-                    cualquier nodo en el montículo, el valor de sus hijos es mayor o igual al suyo.""", unsafe_allow_html=True)
+                    cualquier nodo en el montículo, el valor de sus hijos es mayor o igual al suyo.[1]""", unsafe_allow_html=True)
         st.write("<br><h5>Caracteristicas:</h5>", unsafe_allow_html=True)     
         st.write("""
                     - El elemento de menor valor se encuentra en la raíz del montículo.
                     - Permite acceder rápidamente al mínimo elemento del montículo.
-                    - Para cualquier nodo del montículo, el valor de ese nodo es mayor o igual que el valor de sus nodos descendientes (propiedad del montículo mínimo).
+                    - Para cualquier nodo del montículo, el valor de ese nodo es mayor o igual que el valor de sus nodos descendientes (propiedad del montículo mínimo).[1]
                     """, unsafe_allow_html=True)
 
         st.write("<h5>Codigo en python:</h5>", unsafe_allow_html=True)
@@ -69,13 +69,13 @@ def monticulosMaxMin():
         st.write("""<div style="text-align: justify;">Un montículo máximo es una estructura de datos en 
                     la que el valor de cada nodo es mayor o igual que el valor de sus nodos hijos. Es decir, el nodo raíz 
                     tiene el valor máximo en todo el montículo y para cualquier nodo en el montículo, el valor de sus hijos 
-                    es menor o igual al suyo.</div>""", unsafe_allow_html=True)     
+                    es menor o igual al suyo.[1]</div>""", unsafe_allow_html=True)     
         st.write("<br><h5>Caracteristicas:</h5>", unsafe_allow_html=True)
         st.write("""
                     - El elemento de mayor valor se encuentra en la raíz del montículo.
                     - Permite acceder rápidamente al máximo elemento del montículo.
                     - Para cualquier nodo del montículo, el valor de ese nodo es menor o igual que el valor de sus nodos descendientes (propiedad del montículo máximo).
-                    """, unsafe_allow_html=True)
+                    [1]""", unsafe_allow_html=True)
 
         st.write("<h5>Codigo en python:</h5>", unsafe_allow_html=True)
 
@@ -108,6 +108,8 @@ def importante_saber():
             posiciones 2k y 2k+1 del heap.
         - Un heap puede representarse en un arreglo.
         - Toda lista ordenada es un monticulo.
+        [7]
+
         '''
     )
     # Carateristicas de los monticulos 
@@ -123,16 +125,17 @@ def importante_saber():
         '''
         A partir de esto se pueden clasificar en 
         montículos de orden máximo o de orden mínimo, en donde cada padre de los primeros es mayor 
-        que sus hijos y en los segundos los padres son menores que sus hijos.
+        que sus hijos y en los segundos los padres son menores que sus hijos.[7]
         '''
     )
 
-
+#Funcion para diferenciar un heap y un monticulo 
 def diferencias():
     st.subheader('Como diferenciar un arbol binario de un monticulos(Heap)')
     st.markdown(''' 
                 Un arbol es un conjunto finitos de nodos,ademas puede ser, un conjunto vacio o un conjunto que contiene un nodo raiz y dos arboles disjuntos
                 Algunas de sus caracteristicas que se distinguen de un monticulos son:
+                
                 - Altura: Los árboles son generalmente más altos que otras plantas, como arbustos o hierbas. Tienen un tronco largo y una estructura ramificada en la parte superior.
                 - Ramificación: Los árboles suelen tener ramas que se extienden desde el tronco principal y se subdividen en ramas más pequeñas. Esta estructura ramificada es una característica típica de los árboles.
                 - Raíces: Los árboles tienen raíces bien desarrolladas que se extienden en el suelo para proporcionar soporte y absorber nutrientes. Las raíces suelen ser más grandes y profundas en comparación con otras plantas.
@@ -144,7 +147,7 @@ def diferencias():
                 - Propiedad del montículo: Un montículo cumple con la propiedad del montículo, que puede ser de dos tipos: montículo máximo o montículo mínimo. En un montículo máximo, el valor almacenado en cada nodo es mayor o igual que los valores almacenados en sus nodos hijos. En un montículo mínimo, el valor almacenado en cada nodo es menor o igual que los valores almacenados en sus nodos hijos.
                 - Estructura completa: Un montículo se puede representar mediante un árbol binario completo, lo que significa que todos los niveles del árbol están completamente llenos, excepto posiblemente el último nivel, que se llena de izquierda a derecha. Esta estructura completa permite una representación eficiente del montículo utilizando un arreglo.
                 - Operaciones de inserción y extracción: Los montículos suelen utilizarse para implementar colas de prioridad, ya que permiten la inserción eficiente de elementos y la extracción del elemento de mayor o menor prioridad según el tipo de montículo. Las operaciones de inserción y extracción mantienen la propiedad del montículo.
-                
+                 [2],[3],[5],[7]
                 ''')
 
 
@@ -186,7 +189,7 @@ def tipos():
                     - La unión de dos montículos binomiales se realiza combinando los árboles binomiales del mismo grado en una estructura jerárquica.
                     """, unsafe_allow_html=True)
     
-    with tipo2:
+    with tipo2:#Info Monticulo fibonacci
         st.write("<h3>Montículo fibonacci:</h3>", unsafe_allow_html=True)
         st.write("""<div style="text-align: justify;">Un montículo de Fibonacci es similar a un montículo 
                     binomial, pero permite operaciones más eficientes de unión y disminución de clave. Además, 
@@ -200,7 +203,7 @@ def tipos():
                     - Los árboles de Fibonacci se combinan durante las operaciones de unión para mantener una estructura equilibrada.
                     """, unsafe_allow_html=True)
     
-    with tipo3:
+    with tipo3:#Info Monticulo paridad
         st.write("<h3>Montículo paridad:</h3>", unsafe_allow_html=True)
         st.write("""<div style="text-align: justify;">Un montículo de paridad es una variante de los montículos 
                     binarios, donde los nodos se organizan de acuerdo con su valor y su paridad. Los nodos de paridad 
@@ -213,7 +216,7 @@ def tipos():
                     - La unión de dos montículos de paridad se realiza combinando los árboles en función de la paridad de sus raíces.
                     """, unsafe_allow_html=True)
     
-    with tipo4:
+    with tipo4: #Info Monticulo leftist
         st.write("<h3>Montículo leftist:</h3>", unsafe_allow_html=True)
         st.write("""<div style="text-align: justify;">Un montículo de leftist es un tipo de montículo binario que 
                     se basa en la propiedad de "peso izquierdo". Cada nodo almacena la distancia más corta al nodo más 
@@ -227,7 +230,7 @@ def tipos():
                     - Los árboles leftist se combinan durante las operaciones de unión para mantener la propiedad de leftist y el orden de los valores.
                     """, unsafe_allow_html=True)
     
-    with tipo5:
+    with tipo5:#Info Monticulo par binario 
         st.write("<h3>Montículo par binario:</h3>", unsafe_allow_html=True)
         st.write("""<div style="text-align: justify;">Un montículo de par binario es una variante de los montículos 
                     binarios, donde los nodos se agrupan en pares y el valor de cada nodo del par es menor o igual al 
